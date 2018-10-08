aa873f8348563d94e2889027ff6eb745535cbf52
KeziahMoselle <keziahmoselle@protonmail.com>
2018-10-08 14:33:54 +0200

    Refactor code

--- bot.js
+++ bot.js
@@ -5,6 +5,8 @@
 
 const path = require('path')
 
+const signale = require('signale')
+
 /**
  * REQUIRE
  * Discord related
@@ -13,13 +15,6 @@ const path = require('path')
 const Discord = require('discord.js')
 
 const client = new Discord.Client()
-
-/**
- * REQUIRE
- * HTTP Request
- */
-
-const axios = require('axios')
 
 /**
  * REQUIRE
@@ -33,6 +28,13 @@ const app = express()
 const webPort = process.env.PORT || 80
 
 /**
+ * Commands
+ * Functions
+ */
+
+const commands = require('./commands')
+
+/**
  * LOGIN
  * Bot
  */
@@ -41,130 +43,28 @@ client.login(process.env.DISCORD_KEY)
 
 /**
  * EVENT
- * ready
+ * Discord.js
  */
 
 client.on('ready', () => {
-  console.log('gelbooru-bot is initialized !')
+  signale.success('gelbooru-bot is initialized !')
+})
+
+client.on('message', message => {
+  if (!message.channel.nsfw) {
+    message.reply('Your request must be in a nsfw channel.')
+    return signale.pending('/!\\ Request not in nsfw channel.')
+  }
+  commands.checkHelp(message)
+  commands.checkRequest(message)
 })
 
 client.on('error', (error) => {
-  console.log(error)
+  signale.fatal(new Error(error))
 })
 
 client.on('disconnect', () => {
-  console.log('gelbooru-bot is down.')
-})
-
-/**
- * EVENT
- * On message
- */
-
-client.on('message', message => {
-  if (message.content === '$help') {
-    let embed =
-    {
-      title: 'Commands',
-      fields: [
-        {
-          name: '$',
-          value: 'Search random images'
-        },
-        {
-          name: '$tag',
-          value: 'Search for tag'
-        },
-        {
-          name: '$tag1 tag2',
-          value: 'Search for tag1 and tag2'
-        },
-        {
-          name: '$tag rating:explicit',
-          value: 'Search for tag : Explicit content only'
-        },
-        {
-          name: '$tag rating:questionable',
-          value: 'Search for tag : Questionable content only'
-        },
-        {
-          name: '$tag rating:safe',
-          value: 'Search  for tag : Safe content only'
-        }
-      ]
-    }
-    message.reply('', { embed: embed })
-  } else if (message.content.charAt(0) === '$') {
-    if (!message.channel.nsfw) {
-      message.reply('Your request must be in a nsfw channel.')
-      return console.log('/!\\ Request not in nsfw channel.')
-    }
-    // Get tags from $command
-    let imgLimit = message.content.includes('-5') ? 5 : 1
-    let data = message.content.split('$')
-    let tags = data[1].replace(' ', '+')
-
-    let pid = Math.floor((Math.random() * 5000) + 1)
-    // API GET
-    axios.get(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=${imgLimit}&tags=${tags}&pid=${pid}`)
-      .then((data) => {
-        data.data.forEach(image => {
-          if (image !== undefined) {
-          // If a sample was found send the sample
-            console.log(`${message.author.username} requested '${message.content}' = ${image.file_url}`)
-            let embed = {
-              'title': 'Go to image source on Gelbooru',
-              'description': `You searched for: ${tags}`,
-              'url': `https://gelbooru.com/index.php?page=post&s=view&id=${image.id}`,
-              'color': 44678,
-              'image': {
-                'url': `${image.file_url}`
-              }
-            }
-            message.reply({ embed })
-          } else {
-            axios.get(`https://gelbooru.com/index.php?page=dapi&s=tag&q=index&json=1&name_pattern=${tags}&limit=3&order=DESC&orderby=count`)
-              .then((suggestions) => {
-                let suggestion1 = suggestions.data[0].tag
-
-                let suggestion2 = suggestions.data[1].tag
-
-                let suggestion3 = suggestions.data[2].tag
-
-                let suggestion1count = `${suggestions.data[0].count} results found.`
-
-                let suggestion2count = `${suggestions.data[1].count} results found.`
-
-                let suggestion3count = `${suggestions.data[2].count} results found.`
-
-                let embed =
-                  {
-                    title: `Any results for ${tags}, Here some suggestions :`,
-                    fields: [
-                      {
-                        name: suggestion1,
-                        value: suggestion1count
-                      },
-                      {
-                        name: suggestion2,
-                        value: suggestion2count
-                      },
-                      {
-                        name: suggestion3,
-                        value: suggestion3count
-                      }
-                    ]
-                  }
-
-                message.reply({ embed })
-              })
-          }
-        })
-      })
-      .catch((error) => {
-        message.reply('Sorry, there was an unexpected error. Try with another tags', error)
-      })
-  }
+  signale.fatal(new Error('gelbooru-bot is down.'))
 })
 
 /*
@@ -182,7 +82,7 @@ app.get('/', (request, response) => {
  */
 
 app.listen(webPort, () => {
-  console.log(`Webserver is listening on ${webPort}`)
+  signale.success(`Webserver is listening on ${webPort}`)
 })
 
 /*
@@ -191,3 +91,12 @@ app.listen(webPort, () => {
  */
 
 app.use(express.static(path.join(__dirname, 'views')))
+
+/*
+ * PROCESS
+ * Uncaught Exception
+ */
+
+process.on('uncaughtException', (error) => {
+  signale.fatal(new Error(error))
+})
---
+++ commands.js
@@ -0,0 +1,78 @@
+const axios = require('axios')
+const signale = require('signale')
+
+exports.checkRequest = (message) => {
+  if (message.content.charAt(0) === '$') {
+    // Get tags from $command
+    let imgLimit = message.content.includes('-5') ? 5 : 1
+    let data = message.content.split('$')
+    let tags = data[1].replace(' ', '+')
+
+    let pid = Math.floor((Math.random() * 5000) + 1)
+    // API GET
+    axios.get(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=${imgLimit}&tags=${tags}&pid=${pid}`)
+      .then((data) => {
+        data.data.forEach(image => {
+          if (image !== undefined) {
+            // If a sample was found send the sample
+            let embed = {
+              'title': 'Go to image source on Gelbooru',
+              'description': `You searched for: ${tags}`,
+              'url': `https://gelbooru.com/index.php?page=post&s=view&id=${image.id}`,
+              'color': 44678,
+              'image': {
+                'url': `${image.file_url}`
+              }
+            }
+            message.reply({ embed })
+            signale.success(`${message.author.username} requested '${message.content}' = ${image.file_url}`)
+          }
+        })
+      })
+      .catch((error) => {
+        message.reply('Sorry, there was an unexpected error. Try with another tags', error)
+        signale.fatal(new Error(error))
+      })
+  }
+}
+
+exports.checkHelp = (message) => {
+  if (message.content === '$help') {
+    let embed =
+    {
+      title: 'Commands',
+      fields: [
+        {
+          name: '$',
+          value: 'Search random images (Can be NSFW)'
+        },
+        {
+          name: '$tag',
+          value: 'Search for tag'
+        },
+        {
+          name: '$tag -5',
+          value: 'Search for tag and get 5 results'
+        },
+        {
+          name: '$tag1 tag2',
+          value: 'Search for tag1 and tag2'
+        },
+        {
+          name: '$tag rating:explicit',
+          value: 'Search for tag : Explicit content only'
+        },
+        {
+          name: '$tag rating:questionable',
+          value: 'Search for tag : Questionable content only'
+        },
+        {
+          name: '$tag rating:safe',
+          value: 'Search  for tag : Safe content only'
+        }
+      ]
+    }
+    message.reply('', { embed: embed })
+    signale.success(`${message.author.username} requested ${message.content}`)
+  }
+}
